4b352744cd55e74ec9f02d763b7176e13e91cd52
Anymus <balazsnyirfa@gmail.com>
2019-02-06 01:18:23 +0100

    Making home page to production
    Some menu changes, images and content are changed to prod version, still need some though...
    Show cards randomly with Javascript and added footer with icon links.
    Changed readme description and made the repository public.

---
+++ feedlyf4 web/nav-script.js
@@ -0,0 +1,12 @@
+//Készítette: F4 csoport
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("openMenu").style.display = "none";
+    document.getElementById("closeMenu").style.display = "block";
+    }
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("closeMenu").style.display = "none";
+    document.getElementById("openMenu").style.display = "block";
+    }
--- feedlyf4 web/script.js
+++ feedlyf4 web/script.js
@@ -1,8 +1,76 @@
 //Készítette: F4 csoport
 
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+//declaring element variables
+let unicornCard = document.querySelector("#unicorn-card");
+let phoenixCard = document.querySelector("#phoenix-card");
+let dragonCard = document.querySelector("#dragon-card");
+let sampleCard = document.querySelector("#sample-card");
+
+
+
+//random num function
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+let isVisible = [true, false, true, false];
+
+
+
+
+function changeCard() {
+
+    //declaring random bool variables
+    let isVisible_unicorn = isVisible[getRandomInt(3)];
+    let isVisible_phoenix = isVisible[getRandomInt(3)];
+    let isVisible_dragon = isVisible[getRandomInt(3)];
+    let isVisible_sample = isVisible[getRandomInt(3)];
+
+
+    //decide logic
+
+    //unicorn
+    if (isVisible_unicorn == true) {
+        isVisible_unicorn = "block";
     }
-    function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+    else {
+        isVisible_unicorn = "none";
     }
+
+    //phoenix
+    if (isVisible_phoenix == true) {
+        isVisible_phoenix = "block";
+    }
+    else {
+        isVisible_phoenix = "none";
+    }
+
+    //dragon
+    if (isVisible_dragon == true) {
+        isVisible_dragon = "block";
+    }
+    else {
+        isVisible_dragon = "none";
+    }
+
+    //sample
+    if (isVisible_sample == true) {
+        isVisible_sample = "block";
+    }
+    else {
+        isVisible_sample = "none";
+    }
+
+
+    console.log(isVisible_sample, isVisible_dragon);
+
+
+    unicornCard.style.display = isVisible_unicorn;
+    phoenixCard.style.display = isVisible_phoenix;
+    dragonCard.style.display = isVisible_dragon;
+    sampleCard.style.display = isVisible_sample;
+}
+
+setInterval(function() {
+    changeCard();
+}, 20 * 1000); // 20 * 1000 milsec
